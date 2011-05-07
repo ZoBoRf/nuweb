d3b35d8987c55bfe40c279d97ca5b5b561500fad
nobody <nobody@b730efe3-87cb-4510-a5b4-b26e84f49d7d>
2011-05-07 05:44:56 +0000

    This commit was manufactured by cvs2svn to create tag 'Release_1_56'.

---
+++ tags/Release_1_56/nuweb/test/00/t0016a.sh
@@ -0,0 +1,259 @@
+#!/bin/sh
+#
+# $RCSfile: t0016a.sh,v $-- Test test/00/t0016a.sh
+#
+#
+# Test no pagebreak before scraps
+#
+work=${TMPDIR:-/tmp}/$$
+PAGER=cat
+export PAGER
+umask 022
+here=`pwd`
+if test $? -ne 0 ; then exit 2; fi
+SHELL=/bin/sh
+export SHELL
+
+bin="$here/${1-.}"
+
+pass()
+{
+	set +x
+	cd $here
+	rm -rf $work
+	exit 0
+}
+fail()
+{
+	set +x
+	echo "FAILED test no pagebreak before scraps" 1>&2
+	cd $here
+	rm -rf $work
+	exit 1
+}
+no_result()
+{
+	set +x
+	echo "NO RESULT for test no pagebreak before scraps" 1>&2
+	cd $here
+	rm -rf $work
+	exit 2
+}
+trap \"no_result\" 1 2 3 15
+
+mkdir $work
+if test $? -ne 0 ; then no_result; fi
+cd $work
+if test $? -ne 0 ; then no_result; fi
+
+#
+# test ???
+#
+
+cat > test.w <<"EOF"
+\documentclass{article}
+\begin{document}
+This is a song without and end. It goes on and on my friend. Some
+people started singing it, not knowing what it was, and they'll
+go on forever just because:
+
+This is a song without and end. It goes on and on my friend. Some
+people started singing it, not knowing what it was, and they'll
+go on forever just because:
+
+This is a song without and end. It goes on and on my friend. Some
+people started singing it, not knowing what it was, and they'll
+go on forever just because:
+
+This is a song without and end. It goes on and on my friend. Some
+people started singing it, not knowing what it was, and they'll
+go on forever just because:
+
+This is a song without and end. It goes on and on my friend. Some
+people started singing it, not knowing what it was, and they'll
+go on forever just because:
+
+This is a song without and end. It goes on and on my friend. Some
+people started singing it, not knowing what it was, and they'll
+go on forever just because:
+
+This is a song without and end. It goes on and on my friend. Some
+people started singing it, not knowing what it was, and they'll
+go on forever just because:
+
+This is a song without and end. It goes on and on my friend. Some
+people started singing it, not knowing what it was, and they'll
+go on forever just because:
+
+This is a song without and end. It goes on and on my friend. Some
+people started singing it, not knowing what it was, and they'll
+go on forever just because:
+
+This is a song without and end. It goes on and on my friend. Some
+people started singing it, not knowing what it was, and they'll
+go on forever just because:
+
+This is a song without and end. It goes on and on my friend. Some
+people started singing it, not knowing what it was, and they'll
+go on forever just because:
+
+This is a song without and end. It goes on and on my friend. Some
+people started singing it, not knowing what it was, and they'll
+go on forever just because:
+
+This is a song without and end. It goes on and on my friend. Some
+people started singing it, not knowing what it was, and they'll
+go on forever just because:
+
+This is a song without and end. It goes on and on my friend. Some
+people started singing it, not knowing what it was, and they'll
+go on forever just because:
+
+@c Here comes a block comment just before a scrap and we don't
+want this separated from it. (Thinks:
+This is a song without and end. It goes on and on my friend. Some
+people started singing it, not knowing what it was, and they'll
+go on forever just because:
+This is a song without and end. It goes on and on my friend. Some
+people started singing it, not knowing what it was, and they'll
+go on forever just because:)
+
+@d Here is a scrap.
+@{This is the contents of the scrap.
+
+More stuff, not related to songs without ends.
+
+"This is a song that will get on your nerves,
+   Get on your nerves,
+   Get on your nerves,"
+
+Ad infinitum.
+@}
+
+\end{document}
+EOF
+
+cat > test.expected.tex <<"EOF"
+\newcommand{\NWtarget}[2]{#2}
+\newcommand{\NWlink}[2]{#2}
+\newcommand{\NWtxtMacroDefBy}{Fragment defined by}
+\newcommand{\NWtxtMacroRefIn}{Fragment referenced in}
+\newcommand{\NWtxtMacroNoRef}{Fragment never referenced}
+\newcommand{\NWtxtDefBy}{Defined by}
+\newcommand{\NWtxtRefIn}{Referenced in}
+\newcommand{\NWtxtNoRef}{Not referenced}
+\newcommand{\NWtxtFileDefBy}{File defined by}
+\newcommand{\NWtxtIdentsUsed}{Uses:}
+\newcommand{\NWtxtIdentsNotUsed}{Never used}
+\newcommand{\NWtxtIdentsDefed}{Defines:}
+\newcommand{\NWsep}{${\diamond}$}
+\newcommand{\NWnotglobal}{(not defined globally)}
+\newcommand{\NWuseHyperlinks}{}
+\documentclass{article}
+\begin{document}
+This is a song without and end. It goes on and on my friend. Some
+people started singing it, not knowing what it was, and they'll
+go on forever just because:
+
+This is a song without and end. It goes on and on my friend. Some
+people started singing it, not knowing what it was, and they'll
+go on forever just because:
+
+This is a song without and end. It goes on and on my friend. Some
+people started singing it, not knowing what it was, and they'll
+go on forever just because:
+
+This is a song without and end. It goes on and on my friend. Some
+people started singing it, not knowing what it was, and they'll
+go on forever just because:
+
+This is a song without and end. It goes on and on my friend. Some
+people started singing it, not knowing what it was, and they'll
+go on forever just because:
+
+This is a song without and end. It goes on and on my friend. Some
+people started singing it, not knowing what it was, and they'll
+go on forever just because:
+
+This is a song without and end. It goes on and on my friend. Some
+people started singing it, not knowing what it was, and they'll
+go on forever just because:
+
+This is a song without and end. It goes on and on my friend. Some
+people started singing it, not knowing what it was, and they'll
+go on forever just because:
+
+This is a song without and end. It goes on and on my friend. Some
+people started singing it, not knowing what it was, and they'll
+go on forever just because:
+
+This is a song without and end. It goes on and on my friend. Some
+people started singing it, not knowing what it was, and they'll
+go on forever just because:
+
+This is a song without and end. It goes on and on my friend. Some
+people started singing it, not knowing what it was, and they'll
+go on forever just because:
+
+This is a song without and end. It goes on and on my friend. Some
+people started singing it, not knowing what it was, and they'll
+go on forever just because:
+
+This is a song without and end. It goes on and on my friend. Some
+people started singing it, not knowing what it was, and they'll
+go on forever just because:
+
+This is a song without and end. It goes on and on my friend. Some
+people started singing it, not knowing what it was, and they'll
+go on forever just because:
+
+\begin{flushleft} \small
+\begin{minipage}{\linewidth} Here comes a block comment just before a scrap and we don't
+want this separated from it. (Thinks:
+This is a song without and end. It goes on and on my friend. Some
+people started singing it, not knowing what it was, and they'll
+go on forever just because:
+This is a song without and end. It goes on and on my friend. Some
+people started singing it, not knowing what it was, and they'll
+go on forever just because:)
+
+\par\vspace{\baselineskip}
+\label{scrap1}\raggedright\small
+\NWtarget{nuweb?}{} $\langle\,${\it Here is a scrap.}\nobreak\ {\footnotesize {?}}$\,\rangle\equiv$
+\vspace{-1ex}
+\begin{list}{}{} \item
+\mbox{}\verb@This is the contents of the scrap.@\\
+\mbox{}\verb@@\\
+\mbox{}\verb@More stuff, not related to songs without ends.@\\
+\mbox{}\verb@@\\
+\mbox{}\verb@"This is a song that will get on your nerves,@\\
+\mbox{}\verb@   Get on your nerves,@\\
+\mbox{}\verb@   Get on your nerves,"@\\
+\mbox{}\verb@@\\
+\mbox{}\verb@Ad infinitum.@\\
+\mbox{}\verb@@{\NWsep}
+\end{list}
+\vspace{-1.5ex}
+\footnotesize
+\begin{list}{}{\setlength{\itemsep}{-\parsep}\setlength{\itemindent}{-\leftmargin}}
+\item {\NWtxtMacroNoRef}.
+
+\item{}
+\end{list}
+\end{minipage}\vspace{4ex}
+\end{flushleft}
+\end{document}
+EOF
+
+$bin/nuweb test.w
+if test $? -ne 0 ; then fail; fi
+
+diff -a --context test.expected.tex test.tex
+if test $? -ne 0 ; then fail; fi
+
+#
+# Only definite negatives are possible.
+# The functionality exercised by this test appears to work,
+# no other guarantees are made.
+#
+pass
---
+++ tags/Release_1_56/nuweb/test/00/t0027a.sh
@@ -0,0 +1,127 @@
+#!/bin/sh
+#
+# $RCSfile: t0027a.sh,v $-- Test test/00/t0027a.sh
+#
+#
+# Test of nested global macros
+#
+work=${TMPDIR:-/tmp}/$$
+PAGER=cat
+export PAGER
+umask 022
+here=`pwd`
+if test $? -ne 0 ; then exit 2; fi
+SHELL=/bin/sh
+export SHELL
+
+bin="$here/${1-.}"
+
+pass()
+{
+	set +x
+	cd $here
+	rm -rf $work
+	exit 0
+}
+fail()
+{
+	set +x
+	echo "FAILED test of nested global macros" 1>&2
+	cd $here
+	rm -rf $work
+	exit 1
+}
+no_result()
+{
+	set +x
+	echo "NO RESULT for test of nested global macros" 1>&2
+	cd $here
+	rm -rf $work
+	exit 2
+}
+trap \"no_result\" 1 2 3 15
+
+mkdir $work
+if test $? -ne 0 ; then no_result; fi
+cd $work
+if test $? -ne 0 ; then no_result; fi
+
+#
+# test nested global macros
+#
+
+cat > test.w <<"EOF"
+\documentclass{article}
+\begin{document}
+@o test.actual.c
+@{ First use in global
+@<Frag 1@>
+@}
+@d Frag 1
+@{Base sector line one.
+@}
+@d Frag 4 uses @'thing@'
+@{Access @1
+@}
+@q Frag 5
+@{@<Frag 4 uses @<+Frag 2@>@>
+@}
+@s
+@d Frag 1
+@{First sector line one.
+@}
+@o test.actual.c
+@{@<Frag 3@>
+@}
+@q Frag 3
+@{Use first local
+@<Frag 1@>
+@<+ Frag 2@>
+@}
+@m
+@S
+@d Frag 1
+@{Base sector line two.
+@<Frag 5@>
+@}
+@d+ Frag 2
+@{Here is frag 2
+@}
+@m
+@m+
+\end{document}
+EOF
+
+cat > test.expected.c <<"EOF"
+ First use in global
+Base sector line one.
+Base sector line two.
+@<Frag 4 uses @<+Frag 2@>@>
+
+
+Use first local
+@<Frag 1@>
+@<+Frag 2@>
+
+EOF
+
+# [Add other files here.  Avoid any extra processing such as
+# decompression until after demo has run.  If demo fails this script
+# can save time by not decompressing. ]
+
+$bin/nuweb test.w
+if test $? -ne 0 ; then fail; fi
+
+diff -a --context test.expected.c test.actual.c
+if test $? -ne 0 ; then fail; fi
+
+# [Add other sub-tests that might be failed here.  If they need files
+# created above to be decompressed, decompress them here ; this saves
+# time if demo fails or the text-based sub-test fails.]
+
+#
+# Only definite negatives are possible.
+# The functionality exercised by this test appears to work,
+# no other guarantees are made.
+#
+pass
